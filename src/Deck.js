deba3b0ad649acff96ca5e1a29a18835c06620ba
Prawee Wongsa <prawee@hotmail.com>
2019-04-28 15:23:53 +0700

    create Deck component

---
+++ src/Deck.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Prawee Wongsa 
+ * @Date: 2019-04-28 15:20:45 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2019-04-28 15:21:32
+ */
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Deck;
