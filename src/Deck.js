e84abaa1801a7477801669d5946fbfb3b9c9b3b7
Prawee Wongsa <prawee@hotmail.com>
2019-04-28 16:49:54 +0700

    test data with renderCard

--- src/Deck.js
+++ src/Deck.js
@@ -2,15 +2,22 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-04-28 15:20:45 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-04-28 15:21:32
+ * @Last Modified time: 2019-04-28 16:36:03
  */
 import React, { Component } from 'react';
 import { View, Animated } from 'react-native';
 
 class Deck extends Component {
+  renderCards() {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  }
   render() {
     return (
-      <View />
+      <View>
+        {this.renderCards()}
+      </View>
     );
   }
 }
