cbbafe5f933e90a50bb9cb561e19c5aaa614197d
Prawee Wongsa <prawee@hotmail.com>
2019-04-28 15:24:16 +0700

    using Deck component instead Ball component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Ball from './src/Ball';
+// import Ball from './src/Ball';
+import Deck from './src/Deck';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Ball />
+        <Deck />
       </View>
     );
   }
