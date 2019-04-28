098c6d71313428ff07411a30147d406f96775f0c
Prawee Wongsa <prawee@hotmail.com>
2019-04-28 14:04:35 +0700

    adding animated to ball component

--- src/Ball.js
+++ src/Ball.js
@@ -2,15 +2,24 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-04-28 13:05:01 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-04-28 13:19:30
+ * @Last Modified time: 2019-04-28 14:02:29
  */
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 300, y: 500 }
+    }).start();
+  }
+
   render() {
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>     
     );
   }  
 }
